02e3a2e3402350063c7e3375cc9b1711635c5dcd
Christopher W. Brown <christopherwilliambrown@gmail.com>
2019-04-11 10:03:28 -0400

    add bootstrap styling

--- src/App.js
+++ src/App.js
@@ -10,33 +10,68 @@ class App extends Component {
           <div id="display"> </div>
         </div>
         <div className="row">
-          <button id="clear">AC</button>
-          <button id="divide"> /</button>
+          <button id="clear" className="btn-warning">
+            AC
+          </button>
+          <button id="divide" className="btn-danger">
+            {' '}
+            /
+          </button>
         </div>
         <div className="row">
-          <button id="7">7</button>
-          <button id="8">8</button>
-          <button id="9">9</button>
-          <button id="multiply">X</button>
+          <button id="7" className="btn-primary">
+            7
+          </button>
+          <button id="8" className="btn-primary">
+            8
+          </button>
+          <button id="9" className="btn-primary">
+            9
+          </button>
+          <button id="multiply" className="btn-danger">
+            X
+          </button>
         </div>
 
         <div className="row">
-          <button id="4">4</button>
-          <button id="5">5</button>
-          <button id="6">6</button>
-          <button id="subtract">-</button>
+          <button id="4" className="btn-primary">
+            4
+          </button>
+          <button id="5" className="btn-primary">
+            5
+          </button>
+          <button id="6" className="btn-primary">
+            6
+          </button>
+          <button id="subtract" className="btn-danger">
+            -
+          </button>
         </div>
 
         <div className="row">
-          <button id="1">1</button>
-          <button id="2">2</button>
-          <button id="3">3</button>
-          <button id="add">+</button>
+          <button id="1" className="btn-primary">
+            1
+          </button>
+          <button id="2" className="btn-primary">
+            2
+          </button>
+          <button id="3" className="btn-primary">
+            3
+          </button>
+          <button id="add" className="btn-danger">
+            +
+          </button>
         </div>
         <div className="row">
-          <button id="zero">0</button>
-          <button id="decimal">.</button>
-          <button id="equal">=</button>
+          <button id="zero" className="btn-primary">
+            0
+          </button>
+          <button id="decimal" className="btn-primary">
+            .
+          </button>
+          <button id="equal" className="btn-info">
+            =
+          </button>
         </div>
       </div>
     );
